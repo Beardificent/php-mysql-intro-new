09ef98882cc6de0f1ee2ac40f0c4b9efc05ff776
Beardificent <cis.magito@gmail.com>
2020-10-27 19:03:04 +0100

    protected the Connection class and extended it's functions and methods to User class

--- app/Controller/Connection.php
+++ app/Controller/Connection.php
@@ -5,15 +5,19 @@ class Connection
 {
 
 // XANDER HELPED:  PUT CONNECTION IN FUNCTION SO YOU CAN CALL UPON IT IN ANOTHER FILE (register.php)
-    function openDB()
-    {
-        $dbhost = "localhost";
-        $dbuser = "becode";
-        $dbpass = "becode123";
-        $db = "becode";
 
-        // SET DSN
-        $dsn = 'mysql:host=' . '$dbhost' . ';db=' . $db;
+        private $dbhost;
+        private $dbuser;
+        private $dbpass;
+        private $db;
+
+
+    protected function connect()
+    {
+        $this->dbhost = "localhost";
+        $this->dbuser = "becode";
+        $this->dbpass = "becode123";
+        $this->db = "becode";
 
 
         $driverOptions = [
@@ -22,8 +26,8 @@ class Connection
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         ];
 
-        return new PDO('mysql:host=' . $dbhost . ';dbname=' . $db, $dbuser, $dbpass, $driverOptions);
-
+        $conn = new PDO('mysql:host=' . $this->dbhost . ';dbname=' . $this->db, $this->dbuser, $this->dbpass, $driverOptions);
+        return $conn;
 
     }
 
--- app/Model/User.php
+++ app/Model/User.php
@@ -1,7 +1,22 @@
 <?php
-declare(strict_types=1);
 
-class User
+
+class User extends Connection
 {
+
+    protected function getAllUsers()
+    {
+    $sql = "SELECT * FROM student";
+    $result = $this->connect()->query($sql);
+    $numRows = $result->rowCount();
+    if ($numRows > 0){
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            $data[] = $row;
+        }
+        return $data;
+    }
+
+    }
+
 
 }
