096d96c818982b98c83c4f673b521d5deeee7b88
Beardificent <cis.magito@gmail.com>
2020-10-28 15:47:45 +0100

    worked around the controller error, base page will always load register

--- index.php
+++ index.php
@@ -8,25 +8,26 @@ error_reporting(E_ALL);
 
 //include all your controllers here
 require 'Model/Connection.php';
-require 'Controller/insertController.php';
-
-
-
-
-
 
 //you could write a simple IF here based on some $_GET or $_POST vars, to choose your controller
 //this file should never be more than 20 lines of code!
 
-/*This if(getAllUsers was a public function && have the newer code active) would print out the data from the database.
-$testObj = new getUser();
-$testObj->getAllUsers();
-*/
 
-$controller = new insertController();
+if(isset($_GET['page']) && $_GET['page'] == 'overview'){
+    require 'Controller/OverviewController.php';
+    $controller = new overviewController();
+} elseif (isset($_GET['user'])){
+    require 'Controller/profileController.php';
+    $controller = new ProfileController();
+}else{
+    require 'Controller/InsertController.php';
+    $controller = new insertController();
+}
+
+
 $controller->render($_GET, $_POST);
 
-//meerdere controllers nodig voor meerdere paginas.
+//meerdere controllers nodig voor meerdere paginas. if statements gebruiken?
 
 /*
 //Adjust parameters to highlight specific database entries (example ALL MALE or everyone named BOB)
