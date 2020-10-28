80a51db04ecf22578eb98d4280fb1f4209a45c5f
Beardificent <cis.magito@gmail.com>
2020-10-28 15:28:57 +0100

    added login form

---
+++ View/login.php
@@ -0,0 +1,13 @@
+
+
+<form action="" method="post">
+    <p>
+        <label for="emailAddress">Email Address:</label><br/>
+        <input type="text" name="email" id="emailAddress">
+    </p>
+    <p>
+        <label for="password">Password:</label><br/>
+        <input type="text" name="password" id="password">
+    </p>
+    <input type="submit" value="Submit">
+</form>
